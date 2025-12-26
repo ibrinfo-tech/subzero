6da0bab61c70230d152ee9ef8bc465ac66b00fd7
abhishekibr2 <abhishekibr.trainee2@gmail.com>
2025-12-26 15:07:02 +0530

    Views

--- src/modules/customers/index.ts
+++
@@ -1,9 +0,0 @@
-// Customers module exports
-export * from './types';
-export * from './schemas/customerSchema';
-export * from './schemas/customerValidation';
-export * from './services/customerService';
-export { CustomerForm } from './components/CustomerForm';
-export { CustomerTable } from './components/CustomerTable';
-export { useCustomersCustomFields } from './hooks/useCustomersCustomFields';
-
